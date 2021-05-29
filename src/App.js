deeadcd398367c084b38c83b20af7d8b79cc2723
Vaishak Nair <vaishak.web@gmail.com>
2021-05-30 02:21:54 +0530

    state and setState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import "./App.css";
-import { Greet } from "./components/Greet";
-import Welcome from "./components/Welcome";
-import Hello from "./components/Hello";
-import PropsE from "./components/PropsE";
 import { Component } from "react";
+import Message from "./components/Message";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet />
-        
-        <Hello />
-        <PropsE name="Vaishak" heroName="apple">
-          <p>This is children block</p>
-        </PropsE>
-
-        <Welcome name="Vaishak3" heroName="chicku" />
+        <Message />
       </div>
     );
   }
---
+++ src/components/Message.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Message extends Component {
+    constructor (){
+        super()
+        this.state = {
+            message: 'Welcome visitor'
+        }
+    }
+
+    changeMessage(){
+        this.setState({
+            message: "Thank you for subscribing"
+        })
+    }
+
+  render() {
+    return(
+        <div>
+            <h3>{this.state.message}</h3>
+            <button onClick={() => this.changeMessage()}>Subscribe</button>
+        </div>
+    ) 
+  }
+}
+
+export default Message;
