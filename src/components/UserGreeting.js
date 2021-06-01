c42693b9982ad9d1b7ae351551832144e2d737b0
Vaishak Nair <vaishak.web@gmail.com>
2021-06-02 00:05:44 +0530

    conditional renderings

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -1,41 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class UserGreeting extends Component {
   constructor(props) {
-    super(props)
-  
-    this.state = {
-       isLoggedIn: false
-    }
-  }
-  
-  render() {
-    // Using conditional operator: Approach 4
-    return(
-      <div>
-        {this.state.isLoggedIn ? "Welcome Vaishak" : "Welcome Guest"}
-      </div>
-    )
+    super(props);
 
-    // Using if else outside return using variable: Approach 3
-    //   let message
-    //   if(this.state.isLoggedIn){
-    //     message = <div>Welcome Vaishak</div>
-    //   }else{
-    //     message = <div>Welcome Guest</div>
-    //   }
-    // return <div>{message}</div>
-    
-    // Using if else outside return as JSX doesnot support if else: Approach 2
-    // if(this.state.isLoggedIn){
-    //   return(
-    //     <div>Welcome Vaishak</div>
-    //   )
-    // }else{
-    //   return(
-    //     <div>Welcome Guests</div>
-    //   )
-    // }
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  render() {
+    // Short circuit Operstor: Approach 4
+    return this.state.isLoggedIn && <div>Welcome Vaishak</div>;
+
+    // Using conditional operator: Approach 3
+    return (
+      <div>{this.state.isLoggedIn ? "Welcome Vaishak" : "Welcome Guest"}</div>
+    );
+
+    // Using if else outside return using variable: Approach 2
+    let message;
+    if (this.state.isLoggedIn) {
+      message = <div>Welcome Vaishak</div>;
+    } else {
+      message = <div>Welcome Guest</div>;
+    }
+    return <div>{message}</div>;
+
+    // Using if else outside return as JSX doesnot support if else: Approach 1
+    if(this.state.isLoggedIn){
+      return(
+        <div>Welcome Vaishak</div>
+      )
+    }else{
+      return(
+        <div>Welcome Guests</div>
+      )
+    }
 
     // Normal return without condition: Approach 1
     // return (
@@ -51,4 +52,4 @@ class UserGreeting extends Component {
   }
 }
 
-export default UserGreeting
+export default UserGreeting;
