b39af1e761595ecd7ac3a293ba30c4d7faab5aa9
Vaishak Nair <vaishaknair9@gmail.com>
2021-06-03 00:54:52 +0530

    Update Stylesheet.js

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -4,7 +4,7 @@ function Stylesheet(props) {
     let className = props.primary ? "primary" : ""
     return (
         <div>
-         <h1 className={`${className} font-xl`}>Style</h1>   
+         <h1 className={`${className} font-xl`}>Style Update from git</h1>   
         </div>
     )
 }
