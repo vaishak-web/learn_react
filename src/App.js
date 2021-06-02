f42f44e8502e3b770edf46a0ac01ada4e087b7f0
Vaishak Nair <vaishak.web@gmail.com>
2021-06-03 00:46:14 +0530

    basic styling

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import './components/appStyles.css';
+import styles from './components/appStyles.module.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 className="error">Error</h1>
+        <h1 className={styles.error}>Error</h1>
+        {/* <Stylesheet primary={true}/> */}
+        <Inline/>
         <Counter addValue={1} />
         <Greet name="Vaishak" heroName="SuperMan" />
         <Welcome  name="Vaishak" heroName="SuperMan" />
@@ -23,6 +31,7 @@ class App extends Component {
         <ParentComponent />
         <UserGreeting />
         <NameList />
+        
       </div>
     );
   }
